aa470c618ccf37a1fe60f617b99597e4c8acfad5
epernod <erik.pernod@gmail.com>
2022-10-30 22:25:50 +0100

    [src] Update all files copyright header

--- src/Tearing/TearingAlgorithms.cpp
+++ src/Tearing/TearingAlgorithms.cpp
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ *                 - Copyright (C) - 2020 - InfinyTech3D -                   *
+ *                                                                           *
+ * This file is part of the Tearing plugin for the SOFA framework.           *
+ *                                                                           *
+ * Commercial License Usage:                                                 *
+ * Licensees holding valid commercial license from InfinyTech3D may use this *
+ * file in accordance with the commercial license agreement provided with    *
+ * the Software or, alternatively, in accordance with the terms contained in *
+ * a written agreement between you and InfinyTech3D. For further information *
+ * on the licensing terms and conditions, contact: contact@infinytech3d.com  *
+ *                                                                           *
+ * GNU General Public License Usage:                                         *
+ * Alternatively, this file may be used under the terms of the GNU General   *
+ * Public License version 3. The licenses are as published by the Free       *
+ * Software Foundation and appearing in the file LICENSE.GPL3 included in    *
+ * the packaging of this file. Please review the following information to    *
+ * ensure the GNU General Public License requirements will be met:           *
+ * https://www.gnu.org/licenses/gpl-3.0.html.                                *
+ *                                                                           *
+ * Authors: see Authors.txt                                                  *
+ * Further information: https://infinytech3d.com                             *
+ ****************************************************************************/
 #define SOFA_TEARING_ALGORITHMS_CPP
 #include <Tearing/TearingAlgorithms.inl>
 #include <sofa/defaulttype/VecTypes.h>
