0e632f63565a0e942f7e7622ce606163ed6d4d3e
epernod <erik.pernod@gmail.com>
2024-06-06 17:09:01 +0200

    Fix TearingEngine test

--- Tearing_test/TearingEngine_test.cpp
+++ Tearing_test/TearingEngine_test.cpp
@@ -151,9 +151,9 @@ struct TearingEngine_Case1 : public TearingEngine_test
             m_instance.simulate(0.05);
         }
 
-        EXPECT_EQ(topoCon->getNbTriangles(), 1450);
-        EXPECT_EQ(topoCon->getNbEdges(), 2223);
-        EXPECT_EQ(topoCon->getNbPoints(), 774);
+        EXPECT_EQ(topoCon->getNbTriangles(), 1454);
+        EXPECT_EQ(topoCon->getNbEdges(), 2235);
+        EXPECT_EQ(topoCon->getNbPoints(), 781);
 
         return true;
     }
